bf6e9a1ac43df6f68a20ce32e36268dcf1cb98cc
dwyfrequency <jdwy215@gmail.com>
2018-12-23 21:22:01 -0500

    adding notes for Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,20 +2,23 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
-const concatDuplicate = arr => {
+// Takes array of [ingredient, amount of ingredient]
+const createIngredientArray = arr => {
   const [item, amount] = arr;
+  // if there is only one item return it
   if (amount === 1) {
     return [item];
   } else {
+    // if there are multiple items, see if we have already gone through an iteration and put it in an array. Create new array with item array and an amount for the recursion
     return Array.isArray(item)
-      ? concatDuplicate([item.concat(item[0]), amount - 1])
-      : concatDuplicate([[item].concat(item), amount - 1]);
+      ? createIngredientArray([item.concat(item[0]), amount - 1])
+      : createIngredientArray([[item].concat(item), amount - 1]);
   }
 };
 
 export default props => {
   const transformedIngredientsArray = Object.entries(props.ingredients).reduce(
-    (accum, arr) => accum.concat(...concatDuplicate(arr)),
+    (accum, arr) => accum.concat(...createIngredientArray(arr)),
     []
   );
   const BurgerIngredientJsx = transformedIngredientsArray.map((item, idx) => (
