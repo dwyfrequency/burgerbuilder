6dd53aea16aeac2ce79413a496d4b149dd3e5f42
dwyfrequency <jdwy215@gmail.com>
2019-01-01 23:11:09 -0500

    Adding a Toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Toolbar.module.css";
+
+const Toolbar = props => (
+  <header>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>...</nav>
+  </header>
+);
+
+export default Toolbar;
