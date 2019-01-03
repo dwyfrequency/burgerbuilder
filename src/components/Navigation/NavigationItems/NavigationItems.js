df9cc785455f71c5635aeb66966b3295f6c10b72
dwyfrequency <jdwy215@gmail.com>
2019-01-03 07:59:50 -0500

    Adding Reusable Navigation Items initial draft

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const NavigationItems = props => (
+  <ul>
+    <NavigationItem
+  </ul>
+);
+
+export default NavigationItems;
