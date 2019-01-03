bdf8edd3bfe8fe3dd3b9eeb5cf37785b8c84889e
dwyfrequency <jdwy215@gmail.com>
2019-01-02 21:54:21 -0500

    adding toolbar and styling to layout

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 
 const Toolbar = props => (
-  <header>
+  <header className={classes.Toolbar}>
     <div>MENU</div>
     <div>LOGO</div>
     <nav>...</nav>
