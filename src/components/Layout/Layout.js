68701c1b001bc4c96ee001ad55ffdb6b5be3eab7
dwyfrequency <jdwy215@gmail.com>
2019-01-05 15:22:24 -0500

    adding SideDrawer for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import Aux from "../../hoc/Aux";
+import SideDrawer from "../SideDrawer/SiderDrawer";
 // need to add .module to have it work with reactscripts > 2.0.0
 import classes from "./Layout.module.css";
 const Layout = props => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
